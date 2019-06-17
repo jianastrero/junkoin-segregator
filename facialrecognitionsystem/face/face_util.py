9e31eafdbfdf514ab87bd51a9ead89b780fc98cd
Jian James Astrero <jianjamesastrero@gmail.com>
2019-06-17 21:03:26 +0800

    Add face util

---
+++ facialrecognitionsystem/face/face_util.py
@@ -0,0 +1,55 @@
+#  MIT License
+#
+#  Copyright (c) 2019 Jian James Astrero
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+
+
+import cv2
+
+def cut_faces(image, faces_coord):
+    faces = []
+
+    for (x, y, w, h) in faces_coord:
+        w_rm = int(0.3 * w / 2)
+        faces.append(image[y: y + h, x + w_rm: x + w - w_rm])
+
+    return faces
+
+
+def resize(images, size=(224, 224)):
+    images_norm = []
+    for image in images:
+        if image.shape < size:
+            image_norm = cv2.resize(image, size,
+                                    interpolation=cv2.INTER_AREA)
+        else:
+            image_norm = cv2.resize(image, size,
+                                    interpolation=cv2.INTER_CUBIC)
+        images_norm.append(image_norm)
+
+    return images_norm
+
+
+def normalize_faces(image, faces_coord):
+
+    faces = cut_faces(image, faces_coord)
+    faces = resize(faces)
+
+    return faces
