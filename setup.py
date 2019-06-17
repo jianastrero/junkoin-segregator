a595be4699af8b9420a5020493dc561de9f3dd0d
Jian James Astrero <jianjamesastrero@gmail.com>
2019-06-17 20:43:28 +0800

    require opencv and numpy

--- setup.py
+++ setup.py
@@ -164,7 +164,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],  # Optional
+    install_requires=['opencv-python', 'numpy'],  # Optional
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.
